db8a34f4947b0c6b2280dcc9d2a19172f70f93a6
mcnic16 <jamesmcnic@yahoo.co.uk>
2023-06-08 20:08:16 +0000

    added remove bag view

--- bag/views.py
+++ bag/views.py
@@ -38,3 +38,24 @@ def adjust_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    """Remove the item from the shopping bag"""
+
+    try:
+        basket = None
+        bag = request.session.get('bag', {})
+
+        if basket:
+            del bag[item_id]
+            if not bag[item_id]:
+                bag.pop(item_id)
+        else:
+            bag.pop(item_id)
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
