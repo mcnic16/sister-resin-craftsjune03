dfea93b2d2fdec40ccffe7eff74d3250dac65af5
mcnic16 <jamesmcnic@yahoo.co.uk>
2023-06-18 13:35:31 +0000

    updated admin.py for checkout

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from .models import Order, OrderLineItem
 
-# Register your models here.
-
 
 class OrderLineItemAdminInline(admin.TabularInline):
     model = OrderLineItem
@@ -12,20 +10,19 @@ class OrderLineItemAdminInline(admin.TabularInline):
 class OrderAdmin(admin.ModelAdmin):
     inlines = (OrderLineItemAdminInline,)
 
-    readonly_fields = ('order_number', 'user_profile', 'date',
+    readonly_fields = ('order_number', 'date',
                        'order_total',
-                       'grand_total', 'original_bag',
-                       'stripe_pid')
+                       'grand_total',)
 
     fields = ('order_number', 'date', 'full_name',
               'email', 'phone_number', 'country',
               'postcode', 'town_or_city', 'street_address1',
-              'street_address2', 'county',
-              'order_total', 'grand_total', 'original_bag',
-              'stripe_pid')
+              'street_address2', 'county','order_total', 
+              'grand_total',)
 
     list_display = ('order_number', 'date', 'full_name',
-                    'order_total', 'grand_total',)
+                    'order_total','grand_total',)
 
     ordering = ('-date',)
 
+admin.site.register(Order, OrderAdmin)
