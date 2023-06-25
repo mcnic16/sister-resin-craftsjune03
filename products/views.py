8b7d736adf7d299ddc1235265d6dd855a9f91b26
mcnic16 <jamesmcnic@yahoo.co.uk>
2023-06-25 13:22:56 +0000

    updated product views for superuser

--- products/views.py
+++ products/views.py
@@ -56,6 +56,10 @@ def product_detail(request, product_id):
 @login_required
 def add_product(request):
     """ Add a product to the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only Sister Resin Crafts can do that.')
+        return redirect(reverse('home'))
+
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
@@ -79,7 +83,7 @@ def add_product(request):
 def edit_product(request, product_id):
     """ Edit a product in the store """
     if not request.user.is_superuser:
-        messages.error(request, 'Sorry, only store owners can do that.')
+        messages.error(request, 'Sorry, only Sister Resin Crafts can do that.')
         return redirect(reverse('home'))
 
     product = get_object_or_404(Product, pk=product_id)
@@ -108,7 +112,7 @@ def edit_product(request, product_id):
 def delete_product(request, product_id):
     """ Delete a product from the store """
     if not request.user.is_superuser:
-        messages.error(request, 'Sorry, only store owners can do that.')
+        messages.error(request, 'Sorry, only Sister Resin Crafts can do that.')
         return redirect(reverse('home'))
         
     product = get_object_or_404(Product, pk=product_id)
