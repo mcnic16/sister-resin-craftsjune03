5dc1837f4b464e6f96a64ff660671ef1add1dd32
mcnic16 <jamesmcnic@yahoo.co.uk>
2023-06-26 20:00:29 +0000

    added views for ratings

--- ratings/views.py
+++ ratings/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Rating
 
-# Create your views here.
+def rate_product(request, product_id):
+    if request.method == 'POST':
+        product = get_object_or_404(Product, pk=product_id)
+        user = request.user
+        stars = int(request.POST.get('stars'))
+        comment = request.POST.get('comment')
+
+        Rating.objects.create(product=product, user=user, stars=stars, comment=comment)
+
+        return redirect('ratings:show_ratings')
+
+    return render(request, 'ratings/rate_product.html')
+
+def show_ratings(request):
+    ratings = Rating.objects.all()
+    return render(request, 'ratings/show_ratings.html', {'ratings': ratings})
