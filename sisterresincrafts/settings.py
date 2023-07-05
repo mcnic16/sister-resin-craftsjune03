c02c956dd2bdc40e0dab8c456a9a8e8dd8e194d5
mcnic16 <jamesmcnic@yahoo.co.uk>
2023-07-05 16:53:35 +0000

    added missing files after previous commit to checkout

--- sisterresincrafts/settings.py
+++ sisterresincrafts/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = ')rgn1zk1#+rs)#yl*60v9h)*k-g9ek(p5f++39up%zu++#wkra'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
