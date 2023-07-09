63b1b1f333126d0338cfd52830cd8530c991c37e
mcnic16 <jamesmcnic@yahoo.co.uk>
2023-07-09 09:35:04 +0000

    updated env file for authentication

--- sisterresincrafts/settings.py
+++ sisterresincrafts/settings.py
@@ -27,7 +27,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['sister-resin-craftsjune03-26aff5e95fce.herokuapp.com',
